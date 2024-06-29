b1fb4eb0a5717265e498729516d75efb4048187e
InsanTaufik <insantaufik82@gmail.com>
2024-06-29 11:02:16 +0700

    Minor fix

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, request
-from flask import jsonify
+from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -7,7 +6,7 @@ app = Flask(__name__)
 # MYSQL Config
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'sekolah'
 mysql = MySQL(app)
 
@@ -32,61 +31,55 @@ def guru():
         for row in cursor.fetchall():
             data.append(dict(zip(column_names, row)))
 
-        return jsonify(data)
-
         cursor.close()
+        return jsonify(data)
 
     elif request.method == 'POST':
         nama = request.json['nama']
         mapel = request.json['mata_pelajaran']
-        #Open Connection and Insert to DB
+        # Open Connection and Insert to DB
         cursor = mysql.connection.cursor()
         sql = "INSERT INTO GURU (nama, mata_pelajaran) VALUES (%s, %s)"
         val = (nama, mapel)
         cursor.execute(sql, val)
-        #Commit agar masuk DB
+        # Commit to DB
         mysql.connection.commit()
+        cursor.close()
         return jsonify({'message': 'data added successfully!'})
-        cursor.close()
 
-@app.route('/detailguru')
-def detailguru():
-    if 'id' in request.args:
-        cursor = mysql.connection.cursor()
-        sql = "SELECT * FROM GURU WHERE guru_id = %s"
-        val = (request.args['id'],)
-        cursor.execute(sql,val)
-        column_names = [i[0] for i in cursor.description]
-        data = []
-        for row in cursor.fetchall():
-            data.append(dict(zip(column_names, row)))
-        return jsonify(data)
+@app.route('/detailguru/<int:id>')
+def detailguru(id):
+    cursor = mysql.connection.cursor()
+    sql = "SELECT * FROM GURU WHERE guru_id = %s"
+    val = (id,)
+    cursor.execute(sql, val)
+    column_names = [i[0] for i in cursor.description]
+    data = []
+    for row in cursor.fetchall():
+        data.append(dict(zip(column_names, row)))
+    cursor.close()
+    return jsonify(data)
 
-        cursor.close()
+@app.route('/deleteguru/<int:id>', methods=['DELETE'])
+def deleteguru(id):
+    cursor = mysql.connection.cursor()
+    sql = "DELETE FROM GURU WHERE guru_id = %s"
+    val = (id,)
+    cursor.execute(sql, val)
+    mysql.connection.commit()
+    cursor.close()
+    return jsonify({'message': 'data deleted successfully!'})
 
-@app.route('/deleteguru', methods=['DELETE'])
-def deleteguru():
-    if 'id' in request.args:
-        cursor = mysql.connection.cursor()
-        sql = "DELETE FROM GURU WHERE guru_id = %s"
-        val = (request.args['id'],)
-        cursor.execute(sql, val)
-        mysql.connection.commit()
-        return jsonify({'message': 'data deleted successfully!'})
-        cursor.close()
-
-@app.route('/editguru', methods=['PUT'])
-def editguru():
-    if 'id' in request.args:
-        data = request.get_json()
-        cursor = mysql.connection.cursor()
-        sql = "UPDATE guru SET nama=%s, mata_pelajaran=%s WHERE guru_id = %s"
-        val = (data['nama'], data['mata_pelajaran'], request.args['id'],)
-        cursor.execute(sql, val)
-        
-        mysql.connection.commit()
-        return jsonify({'message': 'data updated successfully!'})
-        cursor.close()
+@app.route('/editguru/<int:id>', methods=['PUT'])
+def editguru(id):
+    data = request.get_json()
+    cursor = mysql.connection.cursor()
+    sql = "UPDATE GURU SET nama=%s, mata_pelajaran=%s WHERE guru_id = %s"
+    val = (data['nama'], data['mata_pelajaran'], id)
+    cursor.execute(sql, val)
+    mysql.connection.commit()
+    cursor.close()
+    return jsonify({'message': 'data updated successfully!'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=50, debug=True)
